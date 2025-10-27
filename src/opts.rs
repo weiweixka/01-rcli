59f137d7e5561901c672df9cf70ecf58b55c21e7
weiweixka <weiweixka666@gmail.com>
2025-10-27 16:58:46 +0800

    json yaml 支持任意csv格式的文件

--- src/opts.rs
+++ src/opts.rs
@@ -1,6 +1,9 @@
+//接收命令行参数
 use clap::Parser;
-use std::path::Path;
+use core::fmt;
+use std::{path::Path, str::FromStr};
 
+//定义命令行参数结构体
 #[derive(Debug, Parser)]
 #[command(name = "rcli", version, author, about, long_about = None)]
 pub struct Opts {
@@ -8,31 +11,83 @@ pub struct Opts {
     pub cmd: SubCommand,
 }
 
+//定义子命令枚举
 #[derive(Debug, Parser)]
 pub enum SubCommand {
     #[command(name = "csv", about = "Show CSV, or convert CSV to other formats")]
     Csv(CsvOpts),
 }
 
+#[derive(Debug, Clone, Copy)]
+pub enum OutputFormat {
+    Json,
+    Yaml,
+}
+
+//定义CSV子命令的参数结构体
 #[derive(Debug, Parser)]
 pub struct CsvOpts {
+    //输入文件参数，必须存在
     #[arg(short, long,value_parser = verfiy_input_file )]
     pub input: String,
 
-    #[arg(short, long, default_value = "output.json")]
-    pub output: String,
+    //输出文件参数，默认值output01.json
+    #[arg(short, long)]
+    pub output: Option<String>,
 
+    //输出格式参数，默认值json
+    #[arg(long, value_parser = parse_format,default_value = "json")]
+    pub format: OutputFormat,
+
+    //分隔符参数，默认值逗号
     #[arg(short, long, default_value_t = ',')]
     pub delimiter: char,
 
+    //是否包含表头参数，默认值true
     #[arg(long, default_value_t = true)]
     pub header: bool,
 }
 
+//验证输入文件是否存在
 fn verfiy_input_file(filename: &str) -> Result<String, String> {
     if Path::new(filename).exists() {
         Ok(filename.into())
     } else {
-        Err("File does not exist".into())
+        Err("文件不存在，请重新选择文件！".into())
+    }
+}
+
+//解析输出格式参数
+fn parse_format(format: &str) -> Result<OutputFormat, anyhow::Error> {
+    format.parse::<OutputFormat>()
+}
+
+//实现From trait以便于将OutputFormat转换为字符串
+impl From<OutputFormat> for &str {
+    fn from(format: OutputFormat) -> Self {
+        match format {
+            OutputFormat::Json => "json",
+            OutputFormat::Yaml => "yaml",
+        }
+    }
+}
+
+//实现FromStr trait以便于从字符串解析OutputFormat
+impl FromStr for OutputFormat {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "json" => Ok(OutputFormat::Json),
+            "yaml" => Ok(OutputFormat::Yaml),
+            _ => Err(anyhow::anyhow!("不支持的输出格式: {}", s)),
+        }
+    }
+}
+
+//实现Display trait以便于格式化输出
+impl fmt::Display for OutputFormat {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", Into::<&str>::into(*self))
     }
 }
--- src/process.rs
+++ src/process.rs
@@ -1,27 +1,50 @@
+use crate::opts::OutputFormat;
 use csv::Reader;
-use serde::{Deserialize, Serialize};
+//use serde::{Deserialize, Serialize};
+use serde_json::Value;
 use std::fs;
 
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all = "PascalCase")]
-struct Player {
-    name: String,
-    position: String,
-    #[serde(rename = "DOB")]
-    dob: String,
-    nationality: String,
-    #[serde(rename = "Kit Number")]
-    kit: u8,
-}
+// //定义与CSV文件对应的结构体(数据表头信息)
+// #[derive(Debug, Serialize, Deserialize)]
+// #[serde(rename_all = "PascalCase")]
+// struct Player {
+//     name: String,
+//     position: String,
+//     #[serde(rename = "DOB")]
+//     dob: String,
+//     nationality: String,
+//     #[serde(rename = "Kit Number")]
+//     kit: u8,
+// }
 
-pub fn process_csv(input: &str, output: &str) -> anyhow::Result<()> {
+//处理CSV文件并转换为JSON格式
+pub fn process_csv(input: &str, output: String, _format: OutputFormat) -> anyhow::Result<()> {
+    //读取CSV文件
     let mut reader = Reader::from_path(input)?;
-    let mut ret: Vec<Player> = Vec::with_capacity(128);
-    for result in reader.deserialize() {
-        let record: Player = result?;
-        ret.push(record);
+    //反序列化CSV记录为Player结构体
+    let mut ret = Vec::with_capacity(128);
+    //获取表头
+    let headers = reader.headers()?.clone();
+    //遍历每一条记录
+    for result in reader.records() {
+        //将记录转换为Player结构体
+        let record = result?;
+        //创建JSON对象
+        //header.iter().zip(record.iter()) 创建一个迭代器，将表头和记录对应起来
+        //zip 方法将两个迭代器组合成一个新的迭代器，生成 (header, record) 对
+        //collect 方法将这些对收集到一个 serde_json::Value 对象中
+        let json_value: Value = headers.iter().zip(record.iter()).collect::<Value>();
+        //将记录添加到结果向量中
+        ret.push(json_value);
     }
-    let json = serde_json::to_string_pretty(&ret)?;
-    fs::write(output, json)?;
+
+    //将结果向量序列化为JSON格式并写入输出文件
+    let content = match _format {
+        OutputFormat::Json => serde_json::to_string_pretty(&ret)?,
+        OutputFormat::Yaml => serde_yaml::to_string(&ret)?,
+    };
+
+    //写入文件
+    fs::write(output, content)?;
     Ok(())
 }
